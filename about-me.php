9fe185e45f3ffdb5b2b27694bc76ddbc943710be
DBarnes0451 <drewbarnes1@hotmail.co.uk>
2020-07-20 12:25:58 +0100

    inital commit

---
+++ about-me.php
@@ -0,0 +1,102 @@
+
+test test test test test
+<?php $title = "PORTFOLIO | MARIA BARNES FREELANCE FASHION DESIGN"; ?>
+<?php $description = "Test Description"; ?>
+<?php include('header.php'); ?>
+<h1>ABOUT ME</h1>
+    <div class="grid-container">
+        <div class="am-grid-container">
+            <div class="am-grid-item-one">
+            <img src="img/maria.png">
+            </div>
+            <div class="am-grid-item-two">
+            <h3 class="about-me-title">WHO AM I&quest;</h3>
+                <div class="about-me-text">
+                    <p>I am passionate about my projects, no matter how large or small, I am highly motivated designer with a wealth of experience in Ladies, Mens & Childrens Essentials: Sleep, Lounge, Lingerie, Swim & Activewear for Retail & On-line retail, inc prints & graphic artworks, creating a great number of Best Sellers.</p>
+                    <p>I have the ability to work through the design process from concept to completion. My organisational skills mean I can work to any critical path & never miss a deadline.</p>
+                    <p>During my career so far I have  been fortunate enough to work with some fantastic Companies & have worked with some great colleagues & clients  most of whom  I am lucky enough to  now call friends.</p>
+                </div>
+            </div>
+            <div class="am-grid-item-three">
+                <h3 class="about-me-title">HOBBIES:</h3>
+                    <div class="about-me-text">
+                        <ul>
+                            <li>Gym Classes</li>
+                            <li>Dog Walking</li>
+                            <li>Music Events</li>
+                            <li>Reading</li>
+                            <li>Travelling</li>
+                            <li>Food</li>
+                        </ul>
+                    </div>
+            </div>
+            <div class="am-grid-item-four">
+                <h3 class="about-me-title">PROFICIENCIES:</h3>
+                    <div class="about-me-text">
+                        <ul>
+                            <li>Illustrator</li>
+                            <li>Range Reviews</li>
+                            <li>Trend Analysis</li>
+</br>
+</br>
+</br>
+                        </ul>
+                    </div>
+            </div>
+            <div class="am-grid-item-five">
+                <h3 class="about-me-title">PREVIOUS ROLES INCLUDE:</h3>
+                    <div class="about-me-text">
+                        <ul>
+                            <li>Senior Designer</li>
+                            <li>Senior Manager</li>
+                            <li>Essentials Designer</li>
+                            <li>Design Consultant</li>
+                            <li>Freelance Designer</li>
+                        </ul>
+                    </div>
+            </div>
+            <div class="am-grid-item-six">
+                <h3 class="about-me-title">PREVIOUS EMPLOYERS INCLUDE:</h3>
+            </div> 
+                <div class="am-grid-item-seven">                                       <img src="img/about-me/n-brown.png" alt="">
+                </div>
+                <div class="am-grid-item-eight">
+                    <img src="img/about-me/express-gifts.png" alt="">
+                </div>
+                <div class="am-grid-item-nine">
+                    <img src="img/about-me/shop-direct.png" alt="">
+                </div>
+                <div class="am-grid-item-ten">
+                    <img src="img/about-me/big-w-logo.png" alt="">
+                </div>
+                <div class="am-grid-item-eleven">
+                    <img src="img/about-me/morrisons-logo.png" alt="">
+                </div>
+                <div class="am-grid-item-twelve">
+                    <img src="img/about-me/matalan.png" alt="">
+                </div> 
+                <div class="am-grid-item-thirteen">
+                    <h3 class="about-me-title">EMPLOYER FEEDBACK:<h3>
+                </div>
+                <div class="am-grid-item-fourteen">
+                    <img src="img/about-me/quote-one.png" alt="">
+                </div>
+                <div class="am-grid-item-fifteen">
+                    <img src="img/about-me/quote-two.png" alt="">
+                </div>
+                <div class="am-grid-item-sixteen">
+                    <img src="img/about-me/quote-three.png" alt="">
+                </div>
+                <div class="am-grid-item-seventeen">
+                    <img src="img/about-me/quote-four.png" alt="">
+                </div> 
+                <div class="am-grid-item-eighteen">
+                    <a href="https://www.linkedin.com/in/maria-barnes-32725063">
+                <div class="about-me-text">SEE MORE</div></a>
+                </div>
+        </div>
+    </div>
+</main>
+</body>
+<?php include('footer.php'); ?>
+</html>
